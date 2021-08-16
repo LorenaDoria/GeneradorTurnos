b1c4bfe436dacd1e2f8c5d06f302b6fe882507ab
LorenaDoria <lorenadoria0426@gmail.com>
2021-08-16 17:06:02 -0500

    Estructura Proyecto

---
+++ src/main/java/com/turnos/proyectoturnos/Usuario.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.turnos.proyectoturnos;
+
+/**
+ *
+ * @author DELL
+ */
+public class Usuario {
+    
+    private String identificacion;
+    private String tipoAfiliacion;
+    private String tipoUsuario ;
+   
+    
+    
+   
+    public Usuario(String identificacion, String tipoAfiliacion, String tipoUsuario){
+        
+        this.identificacion=identificacion;
+        this.tipoAfiliacion=tipoAfiliacion;
+        this.tipoUsuario=tipoUsuario;
+        
+    }
+    
+    
+    public void solicitarTurno(){}
+        
+    
+    
+    
+    
+    
+}
+
+
